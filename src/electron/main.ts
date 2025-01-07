60b3414d6505698be3f132d0ea5a20ab04b91126
Pujan Bhuva <37817715+heisenbug-io@users.noreply.github.com>
2025-01-07 12:58:54 -0500

    Initial setup with DS improvements and scripts
    This can be a good starting point for all the future electron applications.

---
+++ src/electron/main.ts
@@ -0,0 +1,15 @@
+import {
+    app,
+    BrowserWindow
+} from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+
+app.on("ready", () => {
+    let mainWindow = new BrowserWindow({});
+    if(isDev()){
+        mainWindow.loadURL("http://localhost:5123");
+    } else{
+        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+    }
+});
