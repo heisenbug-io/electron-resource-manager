eee2b69ca7267488d47dd51532345704888c1a64
Pujan Bhuva <37817715+heisenbug-io@users.noreply.github.com>
2025-01-07 17:09:44 -0500

    Made electron backend typesafe

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,28 @@
 import {
     app,
-    BrowserWindow
+    BrowserWindow,
+    ipcMain
 } from "electron";
 import path from "path";
-import { isDev } from "./util.js";
+import { ipcMainHandle, isDev } from "./util.js";
+import { pollResources, getStaticData } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-    let mainWindow = new BrowserWindow({});
+    let mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: getPreloadPath(),
+        }
+    });
     if(isDev()){
         mainWindow.loadURL("http://localhost:5123");
     } else{
         mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
     }
+
+    pollResources(mainWindow);
+
+    ipcMainHandle("getStaticData", () => {
+        return getStaticData();
+    })
 });
